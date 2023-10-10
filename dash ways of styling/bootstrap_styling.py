d01f58f75025d3cfb38d8cc2da6c866fe1c01cfc
cooleekova <nushnushka@gmail.com>
2023-10-10 11:47:06 -0300

    add dash styling tutorials

---
+++ dash ways of styling/bootstrap_styling.py
@@ -0,0 +1,56 @@
+""" Dash Bootstrap is a community-maintained library built off of the bootstrap component system. 
+Although it is not officially maintained or supported by Plotly, Dash Bootstrap is a powerful way of building elegant app layouts. 
+Notice that we first define a row and then the width of columns inside the row, using the dbc.Row and dbc.Col components.
+For the app below to run successfully, make sure to install the Dash Bootstrap Components library: pip install dash-bootstrap-components
+"""
+
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+# Incorporate data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+# App layout
+app.layout = dbc.Container([
+    dbc.Row([
+        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+    ]),
+
+    dbc.Row([
+        dbc.RadioItems(options=[{"label": x, "value": x} for x in ['pop', 'lifeExp', 'gdpPercap']],
+                       value='lifeExp',
+                       inline=True,
+                       id='radio-buttons-final')
+    ]),
+
+    dbc.Row([
+        dbc.Col([
+            dash_table.DataTable(data=df.to_dict('records'), page_size=12, style_table={'overflowX': 'auto'})
+        ], width=6),
+
+        dbc.Col([
+            dcc.Graph(figure={}, id='my-first-graph-final')
+        ], width=6),
+    ]),
+
+], fluid=True)
+
+# Add controls to build the interaction
+@callback(
+    Output(component_id='my-first-graph-final', component_property='figure'),
+    Input(component_id='radio-buttons-final', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
